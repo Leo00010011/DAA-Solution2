5d60266dbb4c0efa08a11c0575c3c100ee791eaa
Selleen <97145007+Selleen@users.noreply.github.com>
2023-05-09 16:08:24 -0400

    cambiado de dict a array en optimal

--- optimal_solver.py
+++ optimal_solver.py
@@ -6,7 +6,12 @@ class Vertex:
         self.Id = Vertex.Id
         Vertex.Id = Vertex.Id + 1
         self.neighbourhood : list[(Vertex,int)] = []
-        
+    
+    def __lt__(self, v):
+        return self.Id < v.Id
+    
+    def __eq__(self, v):
+        return self.Id == v.Id
 
 def make_edge(a:Vertex,b:Vertex,weight:int):
     a.neighbourhood.append((b,weight))
@@ -16,7 +21,7 @@ def make_edge(a:Vertex,b:Vertex,weight:int):
 
 def optimal_solver(graph:list[Vertex],principal_vertex:list[Vertex]): 
     #Primero hago Dijkstra para todos los vertices del grafo
-    edges_matrix = []
+    edges_matrix = [[0]*len(principal_vertex) for v in principal_vertex]
     distances_matrix = []
     for vertex in graph:
         vertex.min_edges = [0 for _ in range(len(principal_vertex))]
@@ -26,48 +31,46 @@ def optimal_solver(graph:list[Vertex],principal_vertex:list[Vertex]):
 
     for u in graph:
         for v in graph[u.Id + 1:]:
-            edges_count = graph[v]['array'][u]
+            edges_count = v.min_edges[u.Id]
             for w in graph:
                 if u == w or v == w: continue
-                if distances_matrix[u][w] + distances_matrix[w][v] == distances_matrix[u][v]:
-                    edges_count += graph[w]['array'][u]
-            if u not in edges_matrix:
-                edges_matrix[u] = {}
-            edges_matrix[u][v] = edges_count
+                if distances_matrix[u.Id][w.Id] + distances_matrix[w.Id][v.Id] == distances_matrix[u.Id][v.Id]:
+                    edges_count += w.min_edges[u.Id]
+            edges_matrix[u.Id][v.Id] = edges_count
 
-    for item, edge in zip(distances_matrix.items(), edges_matrix.items()):
-        vertex, distance = item
-        vertex, vert_count_edges = edge
-        #print(f'Distancia desde {vertex} hasta {distance}')
-        #print(f'Cantidad de aristas involucradas desde {vertex} hasta {vert_count_edges}')
-        #print()
+    for vertex, distance  in enumerate(distances_matrix):
+        print(f'Distancia desde {vertex} hasta {distance}')
+        
+        print()
+    for vertex, edge  in enumerate(edges_matrix):
+        print(f'Cantidad de aristas involucradas desde {vertex} hasta {edge}')
+
     return distances_matrix, edges_matrix
 
 
 def dijkstra_modificado(graph, start):
-    distances = {vertex: float('inf') for vertex in graph}
-    distances[start] = 0
-    visited = set()
+    distances = [float('inf') for vertex in graph]
+    distances[start.Id] = 0
+    visited = [False] * len(graph)
 
     priority_queue = [(0, start)]
 
     while priority_queue:
         current_distance, current_vertex = heapq.heappop(priority_queue)
-        if current_vertex in visited:
+        if visited[current_vertex.Id]:
             continue
 
-        visited.add(current_vertex)
+        visited[current_vertex.Id] = True
 
-        for neighbor, weight in graph[current_vertex].items():
-            if neighbor == 'array': continue
+        for neighbor, weight  in current_vertex.neighbourhood:
             distance = current_distance + weight
             
-            if distance < distances[neighbor]:
-                distances[neighbor] = distance
-                graph[neighbor]['array'][start] = 1 
+            if distance < distances[neighbor.Id]:
+                distances[neighbor.Id] = distance
+                neighbor.min_edges[start.Id] = 1 
                 heapq.heappush(priority_queue, (distance, neighbor))
-            elif distance == distances[neighbor]:
-                graph[neighbor]['array'][start] += 1 
+            elif distance == distances[neighbor.Id]:
+                neighbor.min_edges[start.Id]  += 1 
 
     return distances
 
@@ -89,13 +92,18 @@ def dijkstra_modificado(graph, start):
 #    'D': {'A': 0, 'B': 0, 'C': 0, 'D': 0 }
 #}
 graph = []
-for i in 4:
+for i in range(4):
     v = Vertex()
     graph.append(v)
-for v in graph:
-    v.neighbor
 
-distance, edges = optimal_solver(graph)
+make_edge(graph[0], graph[1], 5)
+make_edge(graph[0], graph[2], 2)
+make_edge(graph[1], graph[2], 3)
+make_edge(graph[1], graph[3], 3)
+make_edge(graph[2], graph[3], 6)
+
+
+distance, edges = optimal_solver(graph,graph)
 
 print("Distancia: ", distance)
 print()
